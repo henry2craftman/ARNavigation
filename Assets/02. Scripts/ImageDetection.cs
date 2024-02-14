f16c15e64a6480c70391d373250fbb421f120407
IJHUnity <141684228+henry2craftman@users.noreply.github.com>
2024-02-14 14:04:09 +0900

    feat: Image Detection 기능, UnityWebRequest 데모
    - XR Simulation Image Library 추가
    - ImageDetection.cs Tracking 기능 추가
    - DBManager.cs : WebRequest 데모 추가

---
+++ Assets/02. Scripts/ImageDetection.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// Image Library의 각 이미지에 맞는 3D 오브젝트를 Resources 폴더에서 불러와 생성한다.
+/// </summary>
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class ImageDetection : MonoBehaviour
+{
+    ARTrackedImageManager imageManager;
+    List<GameObject> objs = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        imageManager = GetComponent<ARTrackedImageManager>();
+
+        imageManager.trackedImagesChanged += OnImageTrackedEvent;
+    }
+
+    void OnImageTrackedEvent(ARTrackedImagesChangedEventArgs arg)
+    {
+        foreach(ARTrackedImage trackedImage in arg.added)
+        {
+            string imageName = trackedImage.referenceImage.name;
+
+            GameObject prefab = Resources.Load<GameObject>(imageName);
+
+            if(prefab != null)
+            {
+                GameObject obj = Instantiate(prefab, trackedImage.transform.position, trackedImage.transform.rotation);
+                obj.name = imageName;
+                obj.transform.SetParent(trackedImage.transform);
+                objs.Add(obj);
+            }
+        }
+
+        foreach(ARTrackedImage trackedImage in arg.updated)
+        {
+            if(trackedImage.transform.childCount > 0)
+            {
+                trackedImage.transform.GetChild(0).position = trackedImage.transform.position;
+                trackedImage.transform.GetChild(0).rotation = trackedImage.transform.rotation;
+                trackedImage.transform.GetChild(0).gameObject.SetActive(true);
+            }
+
+            if (trackedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
+            {
+                string imageName = trackedImage.referenceImage.name;
+
+                print(objs.Find(x => x.name == imageName));
+
+                GameObject obj = objs.Find(x => x.name == imageName);
+
+                obj.SetActive(false);
+
+            }
+        }
+
+        foreach (ARTrackedImage trackedImage in arg.removed)
+        {
+            print(trackedImage.transform.childCount);
+            if (trackedImage.transform.childCount > 0)
+            {
+                trackedImage.transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        imageManager.trackedImagesChanged -= OnImageTrackedEvent;
+    }
+}
