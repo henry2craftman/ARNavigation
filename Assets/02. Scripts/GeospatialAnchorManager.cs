85fff15d55510207a51fe32d7c036cc2bd0adc5e
IJHUnity <141684228+henry2craftman@users.noreply.github.com>
2024-02-22 14:51:44 +0900

    feat: CreateAnchor 기능 추가

--- Assets/02. Scripts/GeospatialAnchorManager.cs
+++ Assets/02. Scripts/GeospatialAnchorManager.cs
@@ -13,15 +13,16 @@ public class GeospatialAnchorManager : MonoBehaviour
     public GameObject anchorPrefab;
     AREarthManager earthManager;
     ARAnchorManager anchorManager;
+    GameObject anchoredAsset;
     public float latitude = 33;
     public float longtitude = 126;
     public float altitude = 70;
     public Text logTxt;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        logTxt.text += "시작\n";
+        logTxt.text = "시작";
 
         earthManager = GetComponent<AREarthManager>();
         anchorManager = GetComponent<ARAnchorManager>();
@@ -29,18 +30,38 @@ public class GeospatialAnchorManager : MonoBehaviour
 
     public void SetAnchor()
     {
-        logTxt.text += "시작\n";
-
         if (earthManager.EarthTrackingState == TrackingState.Tracking)
         {
+            string log = "";
+            Vector3 cameraPos = new Vector3((float)earthManager.CameraGeospatialPose.Latitude,
+                                            (float)earthManager.CameraGeospatialPose.Longitude,
+                                            (float)earthManager.CameraGeospatialPose.Altitude);
+            log += $"Camera GeoPose: {cameraPos.x},{cameraPos.y},{cameraPos.z}\n";
+            log += $"Camera VirtualPose: {transform.position}\n";
+
             var anchor =
                 anchorManager.AddAnchor(
                     latitude,
                     longtitude,
                     altitude,
                     Quaternion.identity);
-            var anchoredAsset = Instantiate(anchorPrefab, anchor.transform);
-            logTxt.text += "배치완료: " + anchoredAsset.transform.name + "\n";
+            anchoredAsset = Instantiate(anchorPrefab, anchor.transform);
+            
+            Vector3 anchorPos = new Vector3(latitude, longtitude, altitude);
+            log += $"{anchoredAsset.name} GeoPose: {latitude},{longtitude},{altitude}\n";
+            log += $"{anchoredAsset.name} VirtualPose: {anchoredAsset.transform.position}\n";
+
+            float distance = Mathf.Sqrt(Mathf.Pow(cameraPos.x - latitude, 2) + Mathf.Pow(cameraPos.y - longtitude, 2));
+            log += $"{distance * 100000}m";
+            logTxt.text = log;
+        }
+    }
+    public void ResetAnchor()
+    {
+        if(anchoredAsset != null)
+        {
+            anchoredAsset.transform.SetParent(this.transform);
+            anchoredAsset.transform.localPosition = Vector3.zero + transform.forward;
         }
     }
 }
